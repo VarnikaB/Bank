308f90441a62646b2359192de67a8e0f48a8b1f5
VarnikaB <55554547+VarnikaB@users.noreply.github.com>
2021-01-05 13:37:21 +0530

    Update transfer.php

--- transfer.php
+++ transfer.php
@@ -80,6 +80,7 @@
                 </div>
             </div> 
          </div>
+            <center><a href="index.php"><button class="btn btn-primary" ><b>Home</b></button></a></center>
          <?php require_once "footer.php"; ?>
          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
